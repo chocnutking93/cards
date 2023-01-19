2edfc18078c1d15be0c99eb2400a870150013902
Charles <ckelly218@gmail.com>
2023-01-18 20:51:41 -0800

    Create new function to add click events to an array. Add function to compare array

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const cards = document.querySelectorAll(".cards");
 
-const cardArray = [];
+
 const cardsArray = Array.from(cards);
-console.log(cardsArray);
+// console.log(cardsArray);
 
 let firstCard = false;
 let secondCard;
 let currentTurn;
+let secondPick;
+let pickedCard;
+let arr = []
 
 // const cardArray = [
 //     { name: "1" },
@@ -23,12 +26,12 @@ let currentTurn;
 //     { name: "6" },
 
 // ]
-startGame();
 
 
 function startGame() {
   pickFirstCard()
 
+  // pickSecondCard()
 }
 
 
@@ -41,34 +44,99 @@ function startGame() {
 // }
  
 
-function pickFirstCard() {
-  cardsArray.forEach((card) => {
-    card.addEventListener("click", (e) => {
-      firstCard = !firstCard
-      console.log(`my card is ${e.target.innerText}`);
-      e.target = firstCard;
+// function pickFirstCard() {
+//   cardsArray.forEach((card) => {
+//     card.addEventListener("click", (e) => {
+//       firstCard = !firstCard
+//       pickedCard = e.target.innerText;
+//       console.log(`my first card is ${pickedCard}`);
+//       card.removeEventListener('click', e, true)
+//       pickSecondCard()
 
-      if (firstCard) {
-        isTrue()
-      }
-      else {
-        isFalse()
-      }
-    });
-  });
+//       // console.log(currentTurn.innerText)
+//       // console.log(firstCard ? 'this is my first card' : 'this is my second card')
+
+
+//     });
+//   });
+// }
+
+  cardsArray.forEach(card => {
+  card.addEventListener("click", cardEvent)
+})
+
+
+function pickFirstCard () {
+  cardsArray.forEach(card => {
+    card.addEventListener("click", cardEvent)
+  })
+}
+
+function cardEvent () {
+  firstCard = !firstCard
+  pickedCard = this.innerText;
+  console.log(`my card is ${pickedCard}`)
+  arr.push(pickedCard)
+  if (arr.length === 2) {
+   checkMatch()
+  }
+}
+
+function checkMatch () {
+  if (arr[0] === arr[1]) {
+    console.log('you found a match')
+    arr = []
+    console.log(arr)
+    
+  } else {
+    console.log('try again')
+  }
+}  
+
+
+
+
+ 
+
+function cardEvent2 () {
+  secondPick = this.innerText;
+  console.log(`my second card is ${secondPick}`)  
+
+  cardsArray.forEach(card => {
+    card.removeEventListener("click",cardEvent2)
+  })
+  
 }
 
 
-
-function isTrue () {
-  console.log('this is my first card')
+function pick2nd () {
+  cardsArray.forEach(card => {
+    card.addEventListener("click", cardEvent2)
+  })
 }
 
-function isFalse () {
-  console.log('this is my second card')
-}
+// function pickSecondCard () {
+//   cardsArray.forEach(card => {
+//     card.addEventListener("click", e => {
+//       firstCard = true
+//        secondPick = e.target.innerText;
+//       console.log(`my second card is ${secondPick}`)
+//       card.removeEventListener('click', e)
+//       return secondPick
+//     })
+//   })
+// }
+
+// function switchTurns () {
+//   currentTurn = currentTurn === pickedCard ? secondPick 
+// }
 
 
+// function checkMatch () {
+//   if (currentTurn.innerText === cardsArray[0].innerText) {
+//     console.log('this is a match')
+//   }
+// }
 // function switchTurns () {
 //   currentTurn = currentTurn === firstCard ? secondCard : firstCard;
 // }
