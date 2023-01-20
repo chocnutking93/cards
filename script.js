585bbf836d8e19c10e36b43547f95e1fbbbbb900
Charles <ckelly218@gmail.com>
2023-01-19 22:50:50 -0800

    Add class flip toggle function

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
+
+
 const cards = document.querySelectorAll(".cards");
 const cardsArray = Array.from(cards);
 const notice = document.querySelector(".notice");
+const front = document.querySelectorAll('.front');
+const back = document.querySelectorAll('.back');
+const button = document.querySelector('.btn');
+
 
 let firstCard = false;
 let pickedCard;
 let arr = [];
 const countedArr = [];
 
-cardsArray.forEach((card) => {
-  card.addEventListener("click", cardEvent);
-});
+cards.forEach(card => {
+  card.addEventListener('click',flipCard)
+})
+
+
+// cardsArray.forEach((card) =>  {
+//   card.addEventListener("click", () => {
+//     flipCard();
+//     cardEvent();
+//   });
+// });
 
 function pickFirstCard() {
   cardsArray.forEach((card) => {
-    card.addEventListener("click", cardEvent);
+    card.addEventListener("click", flipCard);
   });
 }
 
@@ -39,3 +53,10 @@ function checkMatch() {
     arr = [];
   }
 }
+
+function flipCard () {
+  console.log(this)
+
+  this.classList.toggle('flipped');
+}
+
